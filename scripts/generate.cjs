804f215d5d9527a7c9b4c457fef8568d6df88f74
Hideaki Kano <kano.hideaki@gmail.com>
2025-03-30 16:33:30 +0900

    Update generate.cjs

--- scripts/generate.cjs
+++ scripts/generate.cjs
@@ -7,7 +7,7 @@ const nunjucks = require('nunjucks');
 nunjucks.configure('templates', { autoescape: true });
 
 const ARTICLES_DIR = 'articles';
-const OUTPUT_DIR = 'docs';
+const OUTPUT_DIR = '.';
 const STATIC_DIR = 'static';
 const ARTICLES_PER_PAGE = 10;
 const START_YEAR = 2009;
